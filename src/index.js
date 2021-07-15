3b0cb5d7b00c5da9cc695af7f32c09a31106438a
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-15 15:09:53 +0100

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* 
+The users and todos variables have all the data for you to work with
+Check your console to see the result of the following console.logs, 
+and inspect the data
+*/
+
+console.log("users: ", users);
+
+console.log("todos: ", todos);
