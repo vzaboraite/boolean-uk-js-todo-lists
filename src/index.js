c393d0848c36a73f2e34ba0030d88a2d8b616cc8
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-15 22:53:34 +0100

    Start challenge1: stuck on displaying todo.id.

--- src/index.js
+++ src/index.js
@@ -44,12 +44,39 @@ const userIdString = prompt(`${requestForId}\n(Please enter an ID)`);
 const userId = parseInt(userIdString);
 
 // Alert with username and todos titles
-let userTodoList = `${users[userId - 1].username} TODOS:\n`;
+/* When user inputs id, it counts from 1, so in order to use user 
+input to reach information within users[], 
+index has to be smaller by one, in this case - users[userId - 1]
+*/
 
+let userTodoList = `${users[userId - 1].username}'s todo list:\n`;
+let username = "";
 for (let i = 0; i < todos.length; i++) {
   if (userId === todos[i].userId) {
+    username = users[userId - 1].username;
     const todoTitle = todos[i].title;
+    // update userTodoList text that will be passed to alert
     userTodoList += `-- ${todoTitle}\n `;
   }
 }
-alert(userTodoList);
+// Add options for user to select from reading or creating the todo list
+const optionString =
+  prompt(`Hello ${username}\nYour Id: ${userId}\nWhat would you like to do?
+1 - Read todo list
+2 - Create new task
+Please type a number`);
+
+const option = parseInt(optionString);
+
+if (option === 1) {
+  alert(userTodoList);
+} else if (option === 2) {
+  let todo = {};
+  todo.userId = userId;
+  //   todo.id = todos[user].id + 1;
+  todo.title = prompt("What task do you need to do?");
+  todo.completed = false;
+  console.log(todo);
+  todos.push(todo);
+}
+console.log(todos);
