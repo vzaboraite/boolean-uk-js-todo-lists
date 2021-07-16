e2f9602b3b19a15d60b40d8a7eabfdc104cf633d
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-16 16:33:04 +0100

    Add update option; needs to be fixed.

--- src/index.js
+++ src/index.js
@@ -48,32 +48,38 @@ const userId = parseInt(userIdString);
 input to reach information within users[], 
 index has to be smaller by one, in this case - users[userId - 1]
 */
-
+const userTodos = [];
 let userTodoList = `${users[userId - 1].username}'s todo list:\n`;
 let username = "";
 for (let i = 0; i < todos.length; i++) {
   if (userId === todos[i].userId) {
-    username = users[userId - 1].username;
-    const todoTitle = todos[i].title;
-    // update userTodoList text that will be passed to alert
-    userTodoList += `-- ${todoTitle}\n `;
+    // username = users[userId - 1].username;
+    // const todoTitle = todos[i].title;
+    // // update userTodoList text that will be passed to alert
+    // userTodoList += `-- ${todoTitle}\n `;
+    userTodos.push(todos[i]);
+    // console.log("todos[i] ", todos[i]);
   }
 }
-// Add options for user to select from reading or creating the todo list
-const optionString =
-  prompt(`Hello ${username}\nYour Id: ${userId}\nWhat would you like to do?
+// Add options for user to select from reading or creating the todo task.
+// Add options for user to select from updating or deleting the todo task.
+const optionString = prompt(`Hello ${
+  users[userId - 1].username
+}!\nYour Id: ${userId}\nWhat would you like to do?
 1 - Read todo list
 2 - Create new task
+3 - Update task
+4 - Delete completed task
 Please type a number`);
 
 const option = parseInt(optionString);
 
 if (option === 1) {
-  alert(userTodoList);
+  alert(todos[i]);
 } else if (option === 2) {
   let highiestId = 0;
-  for (let i = 0; i < todos.length; i++) {
-    const currentId = todos[i].id;
+  for (let i = 0; i < userTodos.length; i++) {
+    const currentId = userTodos[i].id;
     if (currentId >= highiestId) {
       highiestId = currentId + 1;
     }
@@ -84,6 +90,21 @@ if (option === 1) {
   todo.title = prompt("What task do you need to do?");
   todo.completed = false;
   console.log(todo);
-  todos.push(todo);
+  userTodos.push(todo);
+  alert(todo);
+} else if (option === 3) {
+  let taskId = 0;
+  let userTodoListWithId = `What task would you like to uptade?\n`;
+  for (let i = 0; i < userTodos.length; i++) {
+    const id = userTodos[i].id;
+    const title = userTodos[i].title;
+
+    userTodoListWithId += `ID:${id} - ${title}\n`;
+    // console.log("Id ", id);
+    // console.log(userTodoListWithId);
+  }
+  // console.log(userTodoListWithId);
+  prompt(`${userTodoListWithId}\n(Please enter task ID)`);
 }
-console.log(todos);
+
+console.log(userTodos);
